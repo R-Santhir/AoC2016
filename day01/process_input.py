4f37a02a02199a9fc555a42b36af8d789a6805e1
Ryan Santhirarajan <r.santhirarajan@gmail.com>
2016-12-02 00:28:10 -0500

    First attempt, Wrong, 12

---
+++ day01/process_input.py
@@ -0,0 +1,20 @@
+import sys
+from taxiCab import taxiCab
+
+beck = taxiCab(0, 0, 0)
+
+input_file = sys.argv[1]
+with open(input_file,  'r') as f:
+    text = f.read()
+
+directions = text.split(',')
+for move in directions:
+    move = move.strip()
+    if move[0] == 'R':
+        beck.right_turn()
+    else: #'L'
+        beck.left_turn()
+
+    beck.move(int(move[1]))
+
+print (abs(beck.x) + abs(beck.y))
